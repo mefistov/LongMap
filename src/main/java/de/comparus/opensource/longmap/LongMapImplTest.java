147393c68f7ef1f3c7ea153eab0aee95890facef
Mefisto <vlad.seden@gmail.com>
2018-01-14 17:53:24 +0200

    Течническое задание Седень Влад

---
+++ src/main/java/de/comparus/opensource/longmap/LongMapImplTest.java
@@ -0,0 +1,14 @@
+package de.comparus.opensource.longmap;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LongMapImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
